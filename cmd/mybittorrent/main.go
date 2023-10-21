ace3c8b425f02044fb71e070b43ff80c268d4651
Jan Cieślak <jancieslak@interia.eu>
2023-10-21 16:11:34 +0200

    Bencoded list

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 func main() {
@@ -31,4 +34,97 @@ func main() {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
 	}
+}
+
+type Decoder struct {
+	*bytes.Buffer
+}
+
+func NewDecoder(encoded string) *Decoder {
+	return &Decoder{
+		Buffer: bytes.NewBufferString(encoded),
+	}
+}
+
+func (b *Decoder) Decode() (interface{}, error) {
+	return b.decodeBencode()
+}
+
+func (b *Decoder) Read(input []byte) (int, error) {
+	return 0, nil
+}
+
+func (b *Decoder) decodeBencode() (interface{}, error) {
+	prefix, _, err := b.ReadRune()
+	if err != nil {
+		return nil, err
+	}
+	switch {
+	case unicode.IsDigit(prefix):
+		err = b.UnreadRune()
+		if err != nil {
+			return nil, err
+		}
+		return b.decodeString()
+	case prefix == 'i':
+		return b.decodeInteger()
+	case prefix == 'l':
+		return b.decodeList()
+	default:
+		return "", fmt.Errorf("prefix not recognized: %b", prefix)
+	}
+}
+
+func (b *Decoder) decodeString() (string, error) {
+	strLen, err := b.ReadString(':')
+	if err != nil {
+		return "", err
+	}
+	strLen = strLen[:len(strLen)-1]
+
+	intStrLen, err := strconv.Atoi(strLen)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b.Next(intStrLen)), nil
+}
+
+func (b *Decoder) decodeInteger() (int, error) {
+	num, err := b.ReadString('e')
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(num[:len(num)-1])
+}
+
+func (b *Decoder) decodeList() ([]interface{}, error) {
+	list := make([]interface{}, 0)
+
+	var nextRune rune
+	nextRune, _, err := b.ReadRune()
+	if err != nil {
+		return nil, err
+	}
+
+	for nextRune != 'e' {
+		err = b.UnreadRune()
+		if err != nil {
+			return nil, err
+		}
+
+		item, err := b.Decode()
+		if err != nil {
+			return nil, err
+		}
+
+		list = append(list, item)
+
+		nextRune, _, err = b.ReadRune()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return list, nil
 }
